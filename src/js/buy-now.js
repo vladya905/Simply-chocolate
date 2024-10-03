b2b40462e7a27303e629978ce6cfc1680ecdf64d
Antttonio0520 <91532950+Antttonio0520@users.noreply.github.com>
2024-10-03 15:48:06 +0200

    commit

--- src/js/buy-now.js
+++ src/js/buy-now.js
@@ -1,11 +1,11 @@
-const refs = {
-  openMenuBtn: document.querySelector('[data-buynow-open]'),
-  closeMenuBtn: document.querySelector('[data-buynow-close]'),
-  menu: document.querySelector('[data-buynow]'),
-};
-refs.openMenuBtn.addEventListener('click', toggleMenu);
-refs.closeMenuBtn.addEventListener('click', toggleMenu);
-function toggleMenu() {
-  refs.menu.classList.toggle('is-hidden');
-  document.body.classList.toggle('no-scroll');
-}
+// const refs = {
+//   openMenuBtn: document.querySelector('[data-buynow-open]'),
+//   closeMenuBtn: document.querySelector('[data-buynow-close]'),
+//   menu: document.querySelector('[data-buynow]'),
+// };
+// refs.openMenuBtn.addEventListener('click', toggleMenu);
+// refs.closeMenuBtn.addEventListener('click', toggleMenu);
+// function toggleMenu() {
+//   refs.menu.classList.toggle('is-hidden');
+//   document.body.classList.toggle('no-scroll');
+// }
--- src/js/review.js
+++ src/js/review.js
@@ -3,9 +3,10 @@ const refs = {
   closeMenuBtn: document.querySelector('[data-review-close]'),
   menu: document.querySelector('[data-review]'),
 };
+
 refs.openMenuBtn.addEventListener('click', toggleMenu);
 refs.closeMenuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   refs.menu.classList.toggle('is-hidden');
   document.body.classList.toggle('no-scroll');
-}
+};
