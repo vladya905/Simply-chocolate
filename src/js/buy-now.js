4b83ea5bcbf47e72dee7426d4e2c2e1319cb0e0e
Lubajkin Vladislav <125284590+vladya905@users.noreply.github.com>
2024-10-07 00:35:28 +0300

    тюнинг джс 1.0

--- src/js/buy-now.js
+++ src/js/buy-now.js
@@ -1,11 +1,34 @@
 const refs = {
-  openModalBtn: document.querySelector('[data-buynow-open]'),
+  openModalBtns: document.querySelectorAll('[data-buynow-open]'),
   closeModalBtn: document.querySelector('[data-buynow-close]'),
-  menu: document.querySelector('[data-buynow]'),
+  modal: document.querySelector('.modal-buynow'),
+  backdrop: document.querySelector('[data-buynow-backdrop]'),
 };
-refs.openModalBtn.addEventListener('click', toggleModal);
+
+refs.openModalBtns.forEach(btn => {
+  btn.addEventListener('click', toggleModal);
+});
+
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+window.addEventListener('keydown', onEscKeyPress);
+
 function toggleModal() {
-  refs.menu.classList.toggle('is-hidden');
+  refs.modal.classList.toggle('is-hidden');
+  refs.backdrop.classList.toggle('is-hidden');
   document.body.classList.toggle('no-scroll');
-};
+}
+
+function onBackdropClick(event) {
+  if (event.target === refs.backdrop) {
+    toggleModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    if (!refs.modal.classList.contains('is-hidden')) {
+      toggleModal();
+    }
+  }
+}
--- src/js/review.js
+++ src/js/review.js
@@ -1,12 +1,32 @@
 const refs = {
   openModalBtn: document.querySelector('[data-review-open]'),
   closeModalBtn: document.querySelector('[data-review-close]'),
-  menu: document.querySelector('[data-review]'),
+  modal: document.querySelector('[data-review]'),
+  backdrop: document.querySelector('[data-review-backdrop]')
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+window.addEventListener('keydown', onEscKeyPress);
+
+
 function toggleModal() {
-  refs.menu.classList.toggle('is-hidden');
+  refs.modal.classList.toggle('is-hidden');
+  refs.backdrop.classList.toggle('is-hidden');
   document.body.classList.toggle('no-scroll');
-};
+}
+
+function onBackdropClick(event) {
+  if (event.target === refs.backdrop) {
+    toggleModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    if (!refs.modal.classList.contains('is-hidden')) {
+      toggleModal();
+    }
+  }
+}
